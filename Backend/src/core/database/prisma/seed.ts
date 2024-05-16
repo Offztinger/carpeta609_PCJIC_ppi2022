285e0f097cbdddd5c515a13869754ebace160c2d
Alejandro Calvo <offztinger@gmail.com>
2024-05-15 23:40:59 -0500

    Subiendo cambios para semilla

--- Backend/src/core/database/prisma/seed.ts
+++ Backend/src/core/database/prisma/seed.ts
@@ -5,6 +5,7 @@ const prisma = new PrismaClient();
 async function main() {
     await prisma.roles.create({
         data: {
+            id: "3284495c-136e-4215-b8cc-30e6d9ca52b0",
             roleName: "Estudiante",
             roleDescription: "Estudiante",
         },
@@ -12,6 +13,7 @@ async function main() {
 
     await prisma.roles.create({
         data: {
+            id: "1164b212-c28e-4f5c-a886-36795031cbf3",
             roleName: "Profesor",
             roleDescription: "Profesor",
         },
@@ -19,6 +21,7 @@ async function main() {
 
     await prisma.roles.create({
         data: {
+            id: "248d3d75-8e44-4bc6-a0ca-e02bfaa2c0e4",
             roleName: "Asesor",
             roleDescription: "Asesor",
         },
@@ -26,6 +29,7 @@ async function main() {
 
     await prisma.roles.create({
         data: {
+            id: "c27a2360-6bd6-4939-b03c-98e09d25fece",
             roleName: "Coordinador",
             roleDescription: "Coordinador",
         },
