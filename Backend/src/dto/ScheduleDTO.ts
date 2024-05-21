3b7d7b2aa4c654d1e9bf7ad268e9ce544cab9a58
Alejandro Calvo <offztinger@gmail.com>
2024-05-20 22:57:57 -0500

    feat(Schedule): :rocket: Change interface to class in ScheduleDTO

--- Backend/src/dto/ScheduleDTO.ts
+++ Backend/src/dto/ScheduleDTO.ts
@@ -1,8 +1,25 @@
-export interface ScheduleDTO {
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class ScheduleDTO {
     id: string;
+
+    @IsString({ message: 'El número de carpeta debe ser un string' })
+    @IsNotEmpty({ message: 'El número de carpeta no puede estar vacío' })
     folderNumber: string;
+
+    @IsString({ message: 'El nombre de la materia debe ser un string' })
+    @IsNotEmpty({ message: 'El nombre de la materia no puede estar vacío' })
     idProfessor: string;
+
+    @IsString({ message: 'El nombre de la materia debe ser un string' })
+    @IsNotEmpty({ message: 'El nombre de la materia no puede estar vacío' })
     scheduleDate: string;
+
+    @IsString({ message: 'El nombre de la materia debe ser un string' })
+    @IsNotEmpty({ message: 'El nombre de la materia no puede estar vacío' })
     schedulePlace: string;
+
+    @IsString({ message: 'El nombre de la materia debe ser un string' })
+    @IsNotEmpty({ message: 'El nombre de la materia no puede estar vacío' })
     scheduleHour: string;
 }   
