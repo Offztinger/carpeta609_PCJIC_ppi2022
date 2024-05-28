8073960e50652dd37c28ef707e14bc284c1bd49c
Enmanuel Dominguez <denmanuel795@gmail.com>
2024-05-27 23:07:44 -0500

    animaciones modals y distancias en schedulePage

--- Frontend/src/components/shared/UserTable/UserTable.jsx
+++ Frontend/src/components/shared/UserTable/UserTable.jsx
@@ -8,166 +8,187 @@ import download from '../../../icons/downloadfile.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { UserContext } from '../../../context/UserContext/UserContext';
-import usePaginatorHandler from '../../../hooks/paginatorHandler';
 
 function UserTable({ deleteFunction, updateId }) {
-	const [deleteIDEs, setDeleteIDEs] = useState();
-	const [showDeleteModal, setShowDeleteModal] = useState(false);
-	const [showCreateModal, setShowCreateModal] = useState(false);
-	const [currentSection, setCurrentSection] = useState(0);
-	const { handleSectionClick, chunkArray } = usePaginatorHandler();
+    const [deleteIDEs, setDeleteIDEs] = useState();
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [currentSection, setCurrentSection] = useState(0);
 
-	const {
-		users,
-		exportToExcel,
-		selectedId,
-		setFormulario,
-		formulario,
-		handleChange,
-		handleRequestFunction,
-	} = useContext(UserContext);
-	const location = useLocation();
+    const {
+        users,
+        exportToExcel,
+        selectedId,
+        setFormulario,
+        formulario,
+        handleChange,
+        handleRequestFunction,
+    } = useContext(UserContext);
+    const location = useLocation();
 
-	let moduleName = location.pathname.split('/')[1];
+    let moduleName = location.pathname.split('/')[1];
 
-	useEffect(() => {
-		setDeleteIDEs(selectedId);
-	}, [selectedId]);
+    useEffect(() => {
+        setDeleteIDEs(selectedId);
+    }, [selectedId]);
 
-	const validateRole = () => {
-		if (moduleName === 'student') {
-			return '3284495c-136e-4215-b8cc-30e6d9ca52b0';
-		} else if (moduleName === 'professor') {
-			return '1164b212-c28e-4f5c-a886-36795031cbf3';
-		}
-	};
+    const validateRole = () => {
+        if (moduleName === 'student') {
+            return '3284495c-136e-4215-b8cc-30e6d9ca52b0';
+        } else if (moduleName === 'professor') {
+            return '1164b212-c28e-4f5c-a886-36795031cbf3';
+        }
+    };
 
-	const mountEditInfo = user => {
-		updateId(user.id);
-		setShowCreateModal(true);
-		setFormulario({
-			id: user.id,
-			documentNumber: user.documentNumber,
-			name: user.name,
-			lastName: user.lastName,
-			email: user.email,
-			password: user.password,
-			idRole: user.idRole,
-		});
-	};
+    const mountEditInfo = user => {
+        updateId(user.id);
+        setShowCreateModal(true);
+        setFormulario({
+            id: user.id,
+            documentNumber: user.documentNumber,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            password: user.password,
+            idRole: user.idRole,
+        });
+    };
 
-	const secciones = chunkArray(users, 10);
+    const chunkArray = (array, size) => {
+        const result = [];
+        for (let i = 0; i < array.length; i += size) {
+            result.push(array.slice(i, i + size));
+        }
+        return result;
+    };
 
-	return (
-		<div className='contenedorEstudiantes'>
-			<Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
-				<Modal.Header closeButton>
-					<Modal.Title>Eliminar registro</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>Está seguro que desea eliminar este registro?</Modal.Body>
-				<Modal.Footer>
-					<Button
-						variant='secondary'
-						className='btn btn-dark'
-						onClick={() => setShowDeleteModal(false)}
-					>
-						¡No!
-					</Button>
-					<Button
-						variant='primary'
-						className='btn btn-warning'
-						onClick={() => {
-							setShowDeleteModal(false);
-							deleteFunction(deleteIDEs);
-						}}
-					>
-						Sí, deseo eliminarlo
-					</Button>
-				</Modal.Footer>
-			</Modal>
+    const secciones = chunkArray(users, 10);
 
-			<Modal show={showCreateModal} onHide={() => setShowCreateModal(false)}>
-				<Modal.Header closeButton>
-					<Modal.Title>
-						{selectedId ? 'Editar' : 'Crear'}{' '}
-						{moduleName == 'student' ? 'estudiante' : 'profesor'}
-					</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<div className='form-group'>
-						<label>Documento</label>
-						<input
-							type='number'
-							name='documentNumber'
-							className='form-control'
-							value={formulario.documentNumber}
-							onChange={handleChange}
-						/>
-						<small className='form-text text-muted'>
-							Recuerde colocar el documento de identidad sin errores.
-						</small>
-					</div>
-					<div className='form-group'>
-						<label>Nombre</label>
-						<input
-							type='text'
-							name='name'
-							className='form-control'
-							value={formulario.name}
-							onChange={handleChange}
-						/>
-					</div>
-					<div className='form-group'>
-						<label>Apellido</label>
-						<input
-							type='text'
-							name='lastName'
-							className='form-control'
-							value={formulario.lastName}
-							onChange={handleChange}
-						/>
-					</div>
-					<div className='form-group'>
-						<label>Contraseña (opcional)</label>
-						<input
-							type='password'
-							name='password'
-							className='form-control'
-							value={formulario.password}
-							onChange={handleChange}
-						/>
-					</div>
-					<div className='form-group'>
-						<label>Correo Institucional</label>
-						<input
-							type='email'
-							name='email'
-							className='form-control'
-							value={formulario.email}
-							onChange={handleChange}
-						/>
-					</div>
-				</Modal.Body>
-				<Modal.Footer>
-					<Button
-						variant='secondary'
-						className='btn btn-dark'
-						onClick={() => setShowCreateModal(false)}
-					>
-						Cancelar
-					</Button>
-					<Button
-						variant='primary'
-						className='btn btn-success'
-						onClick={() => {
-							handleRequestFunction(moduleName);
-							setShowCreateModal(false);
-						}}
-					>
-						{selectedId ? 'Editar' : 'Crear'}
-					</Button>
-				</Modal.Footer>
-			</Modal>
+    const handleSectionClick = index => {
+        setCurrentSection(index);
+    };
+
+    return (
+        <div className='contenedorEstudiantes'>
+            <Modal
+                show={showDeleteModal}
+                onHide={() => setShowDeleteModal(false)}
+                dialogClassName={showDeleteModal ? 'active' : ''}
+                onEntered={() => document.querySelector('.modal-content').classList.add('active')}
+                onExit={() => document.querySelector('.modal-content').classList.remove('active')}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Eliminar registro</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>¿Está seguro que desea eliminar este registro?</Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant='secondary'
+                        className='btn btn-dark'
+                        onClick={() => setShowDeleteModal(false)}
+                    >
+                        ¡No!
+                    </Button>
+                    <Button
+                        variant='primary'
+                        className='btn btn-warning'
+                        onClick={() => {
+                            setShowDeleteModal(false);
+                            deleteFunction(deleteIDEs);
+                        }}
+                    >
+                        Sí, deseo eliminarlo
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal
+                show={showCreateModal}
+                onHide={() => setShowCreateModal(false)}
+                dialogClassName={showCreateModal ? 'active' : ''}
+                onEntered={() => document.querySelector('.modal-content').classList.add('active')}
+                onExit={() => document.querySelector('.modal-content').classList.remove('active')}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        {selectedId ? 'Editar' : 'Crear'} {moduleName === 'student' ? 'estudiante' : 'profesor'}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className='form-group'>
+                        <label>Documento</label>
+                        <input
+                            type='number'
+                            name='documentNumber'
+                            className='form-control'
+                            value={formulario.documentNumber}
+                            onChange={handleChange}
+                        />
+                        <small className='form-text text-muted'>
+                            Recuerde colocar el documento de identidad sin errores.
+                        </small>
+                    </div>
+                    <div className='form-group'>
+                        <label>Nombre</label>
+                        <input
+                            type='text'
+                            name='name'
+                            className='form-control'
+                            value={formulario.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Apellido</label>
+                        <input
+                            type='text'
+                            name='lastName'
+                            className='form-control'
+                            value={formulario.lastName}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Contraseña (opcional)</label>
+                        <input
+                            type='password'
+                            name='password'
+                            className='form-control'
+                            value={formulario.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Correo Institucional</label>
+                        <input
+                            type='email'
+                            name='email'
+                            className='form-control'
+                            value={formulario.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant='secondary'
+                        className='btn btn-dark'
+                        onClick={() => setShowCreateModal(false)}
+                    >
+                        Cancelar
+                    </Button>
+                    <Button
+                        variant='primary'
+                        className='btn btn-success'
+                        onClick={() => {
+                            handleRequestFunction(moduleName);
+                            setShowCreateModal(false);
+                        }}
+                    >
+                        {selectedId ? 'Editar' : 'Crear'}
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
 			<div className='usersList'>
 				{secciones[currentSection] && (
