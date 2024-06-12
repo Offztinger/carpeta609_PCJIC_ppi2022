b188c2960ab12b1d1137ce53082e582e913388c5
Alejandro Calvo <offztinger@gmail.com>
2024-06-11 20:32:26 -0500

    Arrelglando enlace

--- Frontend/src/config/constants.js
+++ Frontend/src/config/constants.js
@@ -1,3 +1,2 @@
-// export const backendURL = 'https://backend.portalppi.site/';
-export const backendURL = 'http://127.0.0.1:4000/';
+export const backendURL = 'https://backend.portalppi.site/';
 export const estudiante = 'estudiante/';
